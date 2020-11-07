b667ac2378a39de0d27b6aa805584c9b0f92754b
ruid128 <41703844+ruid128@users.noreply.github.com>
2020-11-07 20:13:03 +0300

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -13,25 +13,9 @@ select
       array_agg(xx) 
 from 
     (select 
-          unnest(regexp_split_to_array('Example', E'\\s+')) || ' text' as xx) t;
-          
---Для решения задачи по замене пустых строк на null без сдвига
-update esse_tmp.temp_2
-set col2 = NULLIF(col2, 'l');
+          unnest(regexp_split_to_array('Example', E'\\s+')) || ' text' as xx) t
+;
 
---Решение задачи по поиску и удалению дубликатов строк без свдига по строкам
-update esse_tmp.temp_1
-set col1 = (
-update esse_tmp.temp_1
-set col1 = (select case
-                       when r.t = 1
-                           then r.col1
-                       else null
-                       end as col1
-            from (
-                     select col1, row_number() over (partition by col1) as t
-                     from temp_1) as r
-            );
 
 select distinct
     replace(ВФ,'ВФ:','') f
@@ -79,3 +63,23 @@ from (
         from sch_res.unit001
     ) t
 where COALESCE(n, '') <> ''
+
+          
+--Для решения задачи по замене пустых строк на null без сдвига
+update esse_tmp.temp_2
+set col2 = NULLIF(col2, 'l');
+
+--Решение задачи по поиску и удалению дубликатов строк без свдига по строкам
+update esse_tmp.temp_1
+set col1 = (
+update esse_tmp.temp_1
+set col1 = (select case
+                       when r.t = 1
+                           then r.col1
+                       else null
+                       end as col1
+            from (
+                     select col1, row_number() over (partition by col1) as t
+                     from temp_1) as r
+            );
+
